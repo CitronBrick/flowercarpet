e493e1e4308ce7960bd347fdd073793cd53b818b
CitronBrick <danielanbarasu@gmail.com>
2022-08-28 23:52:53 +0530

    ripple effect added

--- script.js
+++ script.js
@@ -56,6 +56,16 @@ class SimpleFlower extends createjs.Container {
 		this.addChild(core);
 		this.angle = random(360); 
 		this.bloom();
+		var bounds = this.getBounds();
+		this.set({bottomX: this.x + bounds.x + bounds.width, bottomY: this.y + bounds.y + bounds.height});
+		this.set({topX: this.x + bounds.x , topY: this.y + bounds.y });
+	}
+
+	static size = 55;
+
+	isInside(centralArea) {
+		return (this.topX > centralArea.x && this.bottomX < centralArea.bottomX && this.topY > centralArea.y && this.bottomY < centralArea.bottomY )
+
 	}
 
 	bloom() {
@@ -86,10 +96,18 @@ class Tulip extends createjs.Container {
 		// var graphics = bottomPart.graphics.f(color).s(color).mt(-width/2, -100).lt(-width/2,-50).a(0,-50,width/2,0,Math.PI,false).lt(width/2,-50).lt(width/2,-100).lt(-width/2,-100).es().ef();
 		var graphics = bottomPart.graphics.f(color).s(color).mt(-width/2,rectY).lt(-width/2,curveY).a(0,curveY,width/2,0,Math.PI,false);
 		graphics.lt(width/2,curveY).lt(width/2,rectY).lt(-width/2,rectY).es().ef();
-		
-		var petalGroup = drawTriangles({x:-width/2,y:rectY},{x:width/2,y:rectY},3,20,color);
+
+		var nbPetals = random(3)+2;
+		var petalGroup = drawTriangles({x:-width/2,y:rectY},{x:width/2,y:rectY},nbPetals,15,color);
 		this.addChild(bottomPart,petalGroup);
 	}	
+
+	fallSlowly(clbk) {
+		this.set({dx:5, dy:2});
+		createjs.Tween.get(this).to({y:'+300'},2400);
+		var horiTween = createjs.Tween.get(this).to({x:'+25'},1000, createjs.Ease.sineIn);
+		horiTween.to({x:'-25'},1000, createjs.Ease.sineOut).call(clbk);
+	}
 }
 
 function drawTriangles(p1,p2,nb,height,color) {
@@ -117,26 +135,51 @@ function random(n) {
 	return ~~(Math.random() * n);
 }
 
+function randomBetween(a,b) {
+	return a+random(b-a);
+}
+
 function getRandomColor() {
 	return colorList[Math.floor(Math.random()*colorList.length)];
+}
+
+function distance(p1,p2) {
+	return Math.hypot((p1.x-p2.x), (p1.y - p2.y));
 }
 
 class FlowerCarpet extends createjs.Container {
 
 
+	
 	constructor() {
 		super();
 		this.carpetList = [];
-		for(var i = 0; i < 40; i++) {
-			for(var j = 0; j < 50; j++) {
+		for(var i = 0; i < (stage.canvas.width/30 + 1); i++) {
+			for(var j = 0; j < (stage.canvas.height/40+1); j++) {
 				var nbPetals = Math.floor(Math.random()*3) + 4;
-				// var f = new SimpleFlower(i*50,j*60,nbPetals, getRandomColor());
 				var f = new SimpleFlower(i*30,j*40,nbPetals, getRandomColor());
 				this.carpetList.push(f);
 				this.addChild(f);
 			}
 		}
+		this.on('mousedown',this.ripple);
+		this.on('pressmove',this.ripple);
+	}
 
+	ripple=(evt)=> {
+		let x = evt.localX;
+		let y = evt.localY; 
+		let rayon = 30;
+		let flowerList = this.children.filter(f=>{
+			return distance(f,{x,y}) < rayon;
+		})
+		flowerList.forEach((f,i)=>{
+			f.set({initialX: f.x, initialY: f.y});
+			f.set({finalX: ((f.x < x)?'-':'+')+  (rayon-Math.abs(f.x - x)), finalY: ((f.y < y)?'-':'+')+(rayon-Math.abs(f.x - x))});
+			
+			createjs.Tween.get(f,{override: false}).to({x: f.finalX, y: f.finalY},800, createjs.Ease.sineIn).to({x:f.initialX,y:f.initialY},800, createjs.Ease.sineOut);
+			// createjs.Tween.get(f).to({alpha: 0}, 1000).to({alpha: 1}, 1000).wait(400).call(()=>{console.log('ici');this.addFlower(x,y)});
+		})
 	}
 
 
@@ -157,36 +200,52 @@ class FlowerCarpet extends createjs.Container {
 				tw.on('complete', clbk);
 			}
 		});
-		// console.log(cen)/
 	}
+
+	keepBloomingLater(centralArea) {
+		console.log(centralArea);
+		setInterval(()=>{
+			var times = random(2) + 1;
+			for(var i = 0; i < times; i++) {
+				var x = random(stage.canvas.width);
+				var y = random(stage.canvas.height);
+				this.addFlower(x,y);
+			}	
+		},1000);
+	}
+
+	addFlower(x,y) {
+		var centralAreaIndex = stage.children.findIndex((o=>o.name=='centralArea'));
+		var nbPetals = Math.floor(Math.random()*3) + 4;
+		var f = new SimpleFlower(x,y, nbPetals, getRandomColor());
+		if(f.isInside(centralArea)) {
+			return;
+		}
+		this.carpetList.push(f);
+		this.addChild(f);
+		f.bloom();
+	}
+
+
 }
 
 function makeFlowerCarpet() {
 	var carpetList = [];
 	for(var i = 0; i < 40; i++) {
 		for(var j = 0; j < 50; j++) {
-			var nbPetals = Math.floor(Math.random()*3) + 4;
+			var nbPetals = randomBetween(4,7);
 			var f = new SimpleFlower(i*50,j*60,nbPetals, getRandomColor());
 			carpetList.push(f);
 			stage.addChild(f);
 		}
 	}
 
-	/*carpetList.forEach((f,i)=>{
-		createjs.Tween.get(f)
-	})*/
+	
 }
 
 
 
-class Message extends createjs.Container {
 
-	constructor(msg) {
-		super();
-		this.text = new createjs.Text(msg, '20px Arial', 'aquamarine');
-		this.addChild(this.text);
-	}
-}
 
 function makeCentralArea() {
 
@@ -195,23 +254,35 @@ function makeCentralArea() {
 	centralArea.widthRatio = 0.6;
 	centralArea.heightRatio = 0.3;
 
+	var bounds = [0,0, centralArea.widthRatio * stage.canvas.width, Math.max(centralArea.heightRatio * stage.canvas.height, 100)];
+
+
+
 	// var [x,y] = [150,150];
-	var [x,y] = [(stage.canvas.width - centralArea.widthRatio * stage.canvas.width)/2, (stage.canvas.height - stage.canvas.height * centralArea.heightRatio)/2];
-	centralArea.set({x,y});
-	var bounds = [0,0, stage.canvas.width*centralArea.widthRatio, stage.canvas.height* centralArea.heightRatio];
+	// var [x,y] = [(stage.canvas.width - centralArea.widthRatio * stage.canvas.width)/2, (stage.canvas.height - stage.canvas.height * centralArea.heightRatio)/2];
+
+	var [x,y] = [(stage.canvas.width - bounds[2])/2, (stage.canvas.height - bounds[3])/2];
+	centralArea.set({x,y, name:'centralArea'});
 	console.log(bounds);
+	console.log(x,y);
 	centralArea.setBounds(...bounds);
 	var fond = new createjs.Shape();
-	fond.setBounds(0,0,bounds[2],bounds[3]);
-	fond.graphics.f('transparent').dr(0,0,bounds[2],bounds[3]).ef();
-	centralArea.addChild(fond);
+	fond.setBounds(...bounds);
+	fond.graphics.f('gold').dr(0,0,bounds[2],bounds[3]).ef();
+	// centralArea.addChild(fond);
 
 	var mask = new createjs.Shape();
 	mask.set({x,y});
 	mask.graphics.f('red').dr(...bounds).ef();
 	centralArea.mask = mask;
-	console.log(centralArea.children[0]);
-	// console.log(mask);
+
+	var border = new createjs.Shape();
+	border.set({x:0,y:0,name:'border'});
+	border.graphics.ss(15).s('red').dr(0,0,bounds.width, bounds.height).es();
+	// setTimeout(()=>{
+		centralArea.addChild(border);
+		console.log('added');
+	// },1000);
 	return centralArea;
 }
 
@@ -221,26 +292,86 @@ function makeFallingTulips(centralArea) {
 			// let tulip = new Tulip(i*30,  j*-50);
 			var color = colorList[random(colorList.length)];
 			let tulip = new Tulip(j*30,  i*-50, color);
-			tulip.scale = (4+random(3))/10;
+			tulip.scale = (randomBetween(3,7))/10;
 			centralArea.addChild(tulip);		
 			var speed = (random(20)/10 + 5)/10; 
 			var time =( 300-i*-50 )/speed;
-			createjs.Tween.get(tulip).to({y:380},time);
+			var tw = createjs.Tween.get(tulip).to({y:380},time).call(()=>{centralArea.removeChild(tulip)});
+			if(i == 9 && j == 39) { // last iteration
+				tw.call(()=>{
+					addMessage(centralArea);
+					makeSlowFallingTulips(centralArea);
+				})
+			}
 		}
 	}
 }
 
+function makeSlowFallingTulips(centralArea) {
+	var bounds = centralArea.getTransformedBounds();
+	setInterval(()=> {
+		var times = random(5);
+		for(var k = 0; k < times; k++ ) {
+			var color = 'mistyrose'; //getRandomColor();
+			let tulip = new Tulip(random(bounds.width), 0, color);
+			tulip.scale = (1+random(3))/10;
+			centralArea.addChild(tulip);		
+			tulip.fallSlowly(()=>centralArea.removeChild(tulip));
+		}
+	},500);
+}
+
+function addMessage(centralArea) {
+	var msg = getMessageFromQueryString() || 'Happy Wedding Anniversary';
+	msg = msg.replace(/ /g,'\n');
+	var text= new createjs.Text(msg, 'bold 3vh Script MT','RebeccaPurple');
+	text.textbaseLine = 'middle';
+	text.textAlign = 'center';
+	text.shadow = new createjs.Shadow('teal',5,5,10);
+	var metrics = text.getMetrics();
+	// text.lineHeight = 10;
+	console.log(metrics)
+	var centralAreaBounds = centralArea.getBounds();
+	text.x = (centralAreaBounds.width)/2;
+	text.y = (centralAreaBounds.height-metrics.height)/2 ;
+	console.log(text.x,text.y);
+	centralArea.addChild(text);
+}
+
+function getMessageFromQueryString() {
+	var search = window.location.search;
+	if(search) {
+		var usp = new URLSearchParams(search);
+		let msg = usp.get('text');
+		if(msg) {
+			return msg;
+		}
+	}
+	return '';
+}
 
 
-var tulip,centralArea;
+var tulip,centralArea,canvas;
 window.addEventListener('load',()=>{
 	createjs.MotionGuidePlugin.install();
-	var canvas = document.querySelector('canvas');
-
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-
+	createjs.RelativePlugin.install();
+	canvas = document.querySelector('canvas');
 	stage = new createjs.Stage(canvas);
+	createjs.Touch.enable(stage);
+	setup();
+	createjs.Ticker.on('tick', stage);
+
+	window.addEventListener('orientationchange',()=>{
+		stage.removeAllChildren();
+		setup();
+	});
+	
+});
+
+function setup() {
+	stage.canvas.width = window.innerWidth;
+	stage.canvas.height = window.innerHeight;
+
 
 	tulip = new Tulip(200,500,'crimson');
 	tulip.scale=1;
@@ -250,30 +381,18 @@ window.addEventListener('load',()=>{
 
 	// black background
 	var bg = new createjs.Shape();
-	bg.graphics.f('black').dr(0,0,canvas.width,canvas.height).ef();
+	bg.graphics.f('azure').dr(0,0,canvas.width,canvas.height).ef();
 
 	var fc = new FlowerCarpet();
-	stage.addChild(bg, tulip,sf1, new Message("HML"), fc);
+	stage.addChild(bg, sf1, fc);
 
 
 	centralArea = makeCentralArea();
 	fc.moveAwayFrom(centralArea,()=> {
 		makeFallingTulips(centralArea);
+		fc.keepBloomingLater();
 	});
+	// stage.addChildAt(centralArea,stage.children.length-1);
+	stage.addChild(centralArea);	
 
-
-	stage.addChild(centralArea);
-
-
-
-	
-
-	
-
-
-
-
-	
-	createjs.Ticker.on('tick', stage);
-	
-});
+}
